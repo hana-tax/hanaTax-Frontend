809595d8e012c8890578e21838dd4c751da7a781
wldnjs7064 <wldnjs7064@naver.com>
2024-08-16 14:32:30 +0900

    Feat: 연말정산 솔루션 안내

--- src/pages/inquiry/result/ResultYearEnd.js
+++ src/pages/inquiry/result/ResultYearEnd.js
@@ -4,11 +4,19 @@ import "../../../assets/css/InquiryResult.css";
 import "react-custom-alert/dist/index.css";
 import { ReactComponent as Char } from "../../../assets/svg/연말정산/sun.svg";
 import { ReactComponent as Info } from "../../../assets/svg/Info.svg";
+import creditCardIcon from "../../../assets/svg/연말정산/credit-card.svg";
+import userIcon from "../../../assets/svg/연말정산/user.svg";
+import houseIcon from "../../../assets/svg/연말정산/house.svg";
+import briefcaseIcon from "../../../assets/svg/연말정산/briefcase.svg";
+import piggyBankIcon from "../../../assets/svg/연말정산/piggy-bank.svg";
 
 const ResultYearEnd = () => {
   const [amount, setAmount] = useState(0);
   const navigate = useNavigate();
   const targetAmount = 780000;
+  const ToSolutionYearEnd = () => {
+    navigate("/solution/yearend");
+  };
 
   useEffect(() => {
     const duration = 2000;
@@ -51,9 +59,56 @@ const ResultYearEnd = () => {
           <Char />
         </div>
       </div>
+
       <div className="description">
         <div className="description-text">위 금액은 예상납부금입니다.</div>
         <Info />
+      </div>
+
+      <div className="solution-container">
+        <div className="solutions-section">
+          <h2>
+            13월의 월급을 <span className="highlight">더 많이</span> 받을 수
+            있는 <br></br>방법을 알려드릴게요!
+          </h2>
+          <div className="slider">
+            <div className="slide-track">
+              <div className="slide">
+                <img src={creditCardIcon} alt="Credit Card" />
+              </div>
+              <div className="slide">
+                <img src={userIcon} alt="User" />
+              </div>
+              <div className="slide">
+                <img src={houseIcon} alt="House" />
+              </div>
+              <div className="slide">
+                <img src={briefcaseIcon} alt="Briefcase" />
+              </div>
+              <div className="slide">
+                <img src={piggyBankIcon} alt="Piggy Bank" />
+              </div>
+              <div className="slide">
+                <img src={creditCardIcon} alt="Credit Card" />
+              </div>
+              <div className="slide">
+                <img src={userIcon} alt="User" />
+              </div>
+              <div className="slide">
+                <img src={houseIcon} alt="House" />
+              </div>
+              <div className="slide">
+                <img src={briefcaseIcon} alt="Briefcase" />
+              </div>
+              <div className="slide">
+                <img src={piggyBankIcon} alt="Piggy Bank" />
+              </div>
+            </div>
+          </div>
+          <button className="solution-button" onClick={ToSolutionYearEnd}>
+            솔루션 보러가기
+          </button>
+        </div>
       </div>
     </div>
   );
