75b89bd95a7f214d0bc806e363457b700f966fe5
wldnjs7064 <wldnjs7064@naver.com>
2024-09-28 13:27:13 +0900

    Feat: 서버 테스트

--- src/pages/Login/LoginForm.js
+++ src/pages/Login/LoginForm.js
@@ -21,7 +21,7 @@ const LoginForm = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:8080/api/user/login", {
+      const response = await fetch("/api/user/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
