246e9c3b968eee53d9d1aab80db96a6ac2f3894d
wldnjs7064 <wldnjs7064@naver.com>
2024-09-29 12:28:23 +0900

    Fix: deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,2 @@
-#!/bin/bash
-cd /home/ubuntu/hanaTax-Frontend
-git pull origin main  # 최신 코드 가져오기
-npm install --prefer-offline --no-audit --progress=false  # 의존성 설치
-npm run build  # 빌드 실행
+#!/usr/bin/env bash
+ echo "> FE 배포"
